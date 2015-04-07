e42bc03947be17927a3db2f2c640b61e22349028
schleichardt <michael.schleichardt@commercetools.de>
2015-04-07 09:59:46 +0200

    Add tests for future/exception handling demo.

---
+++ test/CompletableFutureTest.java
@@ -0,0 +1,95 @@
+import org.junit.Test;
+import play.libs.F;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Function;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class CompletableFutureTest {
+    @Test
+    public void functionGetWithRuntimeException() throws Exception {
+        final Function<String, Integer> f = s -> {
+            throw new IllegalArgumentException("whatever");
+        };
+        executionThrows(() -> CompletableFuture.completedFuture("hello").thenApply(f).get(),
+                ExecutionException.class, IllegalArgumentException.class, "whatever");
+    }
+
+    @Test
+    public void functionJoinWithRuntimeException() throws Exception {
+        final Function<String, Integer> f = s -> {
+            throw new IllegalArgumentException("whatever");
+        };
+        executionThrows(() -> CompletableFuture.completedFuture("hello").thenApply(f).join(),
+                CompletionException.class, IllegalArgumentException.class, "whatever");
+    }
+
+    @Test
+    public void functionGetWithCheckedException() throws Exception {
+        final Function<String, Integer> f = s -> {
+            throw new CompletionException(new IOException("whatever"));
+        };
+        executionThrows(() -> CompletableFuture.completedFuture("hello").thenApply(f).get(),
+                ExecutionException.class, IOException.class, "whatever");
+    }
+
+    @Test
+    public void functionJoinWithCheckedException() throws Exception {
+        final Function<String, Integer> f = s -> {
+            throw new CompletionException(new IOException("whatever!"));
+        };
+        executionThrows(() -> CompletableFuture.completedFuture("hello").thenApply(f).join(),
+                CompletionException.class, IOException.class, "whatever!");
+    }
+
+    @Test
+    public void promise() throws Exception {
+        final F.Promise<String> promise = F.Promise.throwing(new IOException("whatever!"));
+        executionThrows(() -> promise.get(0), IOException.class, "whatever!");
+    }
+
+    @Test
+    public void promise2() throws Exception {
+        //F.Function (not java.util.function.Function) declares throws Throwable!!
+        final F.Function<String, Integer> f = s -> {
+            throw new IOException("whatever");
+        };
+        final F.Promise<Integer> promise = F.Promise.pure("hi").map(f);
+        executionThrows(() -> promise.get(0), IOException.class, "whatever");//easier handling
+    }
+
+    private <T> void executionThrows(final SupplierWithThrows<T> supplier,
+                                     final Class<? extends Exception> expectedExceptionClass,
+                                     final Class<? extends Exception> expectedExceptionCauseClass,
+                                     final String expectedMessage) {
+        try {
+            supplier.get();
+            throw new RuntimeException("should fail");
+        } catch (final Exception e) {
+            assertThat(e).isInstanceOf(expectedExceptionClass);
+            assertThat(e.getCause()).isInstanceOf(expectedExceptionCauseClass);
+            assertThat(e.getCause().getMessage()).isEqualTo(expectedMessage);
+        }
+    }
+
+    private <T> void executionThrows(final SupplierWithThrows<T> supplier,
+                                     final Class<? extends Exception> expectedExceptionClass,
+                                     final String expectedMessage) {
+        try {
+            supplier.get();
+            throw new RuntimeException("should fail");
+        } catch (final Exception e) {
+            assertThat(e).isInstanceOf(expectedExceptionClass);
+            assertThat(e.getMessage()).isEqualTo(expectedMessage);
+        }
+    }
+
+    @FunctionalInterface
+    private static interface SupplierWithThrows<T> {
+        T get() throws Exception;
+    }
+}
